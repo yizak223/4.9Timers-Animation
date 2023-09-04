a74db729b25da38c15149470e0e52771ebb3e755
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-04 19:23:53 +0300

    commit

--- main.js
+++ main.js
@@ -1,50 +1,25 @@
-document.body.innerHTML += `
-<div id='mainDiv'></div>
-`
-// //1
-// setTimeout(printName, 1000)
+document.body.innerHTML+=`
+<div id='mainDiv'></div>`
+document.body.innerHTML+=`<div id='secondDiv'></div>`
 
-// function printName() {
-//     mainDiv.innerHTML += `
-//     <p>yizak</p>`
-// }
-// //2
-// setTimeout(printHellow, 5000)
-// function printHellow() {
-//     mainDiv.innerHTML = `
-//     <p>hellow</p>
-//     `
-// }
-// mainDiv.innerHTML = `
-//     <p>bye</p>
-//     `
-// //3
-// setTimeout(printTime,1000)
-
-// function printTime() {
-//     var time= new Date()
-//     mainDiv.innerHTML +=`
-//     <span>${time.getHours()}</span>
-//     `
-// }
-//4
-var timeoutText=setTimeout(printText,2000)
-
-function printText() {
-    mainDiv.innerHTML +=`
-    <p>i love timers</p>
-    `
-}
-
-mainDiv.innerHTML+=
-`<button id='deleteSpan'>delete</button>`
-deleteSpan.addEventListener('click',function () {clearTimeout(timeoutText)})
-
-//5
 mainDiv.innerHTML+=`
-<h1 id='title4' style='color:blue'>title</h1>
-`
-function colorRed() {
-    title4.style=`color:red`
+<input id='ipnutUser' type='number'>`
+var num;    
+var timerUser=setInterval(printTimerMinutes,1000)
+ipnutUser.addEventListener('change',getMinutes)
+function getMinutes() {
+    num=ipnutUser.value
+    timerUser
 }
-setTimeout(colorRed,3000)
+function printTimerMinutes() {
+    if(num>0){
+        console.log(num);
+         mainDiv.innerHTML=`
+    ${--num}`
+    }
+}
+
+
+secondDiv.innerHTML=`<br><button id='deleteTimer' onclick='deleteInterval()'>stop</button>`
+function deleteInterval() {clearInterval(timerUser)}
+
