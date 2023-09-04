78869733eade43f9fec8966a458ea29dd4bc514c
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-04 21:42:50 +0300

    commit

--- intervalTask/main.js
+++ intervalTask/main.js
@@ -4,23 +4,23 @@ document.body.innerHTML+=`<div id='secondDiv'></div>`
 
 mainDiv.innerHTML+=`
 <input id='ipnutUser' type='number'>`
-var num;    
+var num;  
+var num2  
 var timerUser=setInterval(printTimerMinutes,1000)
 // var timerUserLeft=setInterval(printTimerMinutesLeft,1000)
 ipnutUser.addEventListener('change',getMinutes)
 function getMinutes() {
     num=ipnutUser.value
-    timerUserף
+    timerUser
      
 }
-//  function contineuTimer(){
-//         var timeLeft=num
-//         timerUserLeft
-//     } 
+ function contineuTimer(){
+        setInterval(printTimerMinutes,1000)
+    } 
 function printTimerMinutes() {
-    if(ipnutUser.value>0){
+    if(num>0){
          mainDiv.innerHTML=`
-    ${--ipnutUser.value}`
+    ${--num}`
     }
 }
 // function printTimerMinutesLeft(){
@@ -34,5 +34,5 @@ secondDiv.innerHTML=`<br><button id='deleteTimer' onclick='deleteInterval()'>sto
 <br><button id='continueBtn' >continue</button>`
 
 function deleteInterval() {clearInterval(timerUser)}
-continueBtn.addEventListener('click',printTimerMinutes)
+continueBtn.addEventListener('click',contineuTimer)
 // var continueUser= setInterval(printTimerMinutesContinue,1000)
